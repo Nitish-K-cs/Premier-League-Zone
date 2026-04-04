1963d04adff7d1a313da09fb5f8b85aa73b371a0
Nitish k <nitish258k@gmail.com>
2026-04-04 18:19:46 +0530

    Springboot

---
+++ IPL-Zone/src/test/java/com/ipl/IPL_Zone/IplZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipl.IPL_Zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
